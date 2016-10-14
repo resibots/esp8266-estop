de07cad8057c98bb0c0c6a8131ffaaf7ff19de16
Dorian Goepp <dorian.goepp@inria.fr>
2016-10-14 10:40:33 +0200

    Finish commands and add help message

--- src/simple_http_client.cpp
+++ src/simple_http_client.cpp
@@ -10,16 +10,19 @@ IPAddress staticIP(152, 81, 70, 17);
 IPAddress gateway(152, 81, 70, 1);
 IPAddress subnet(255, 255, 255, 0);
 
+// Seed for the pulses sent to the ROS node
+uint16_t pulse_seed;
+
 // Create the class for UDP communication
 WiFiUDP Udp;
-char packet_buffer[255]; // buffer for incoming data;
+char packet_buffer[512]; // buffer for incoming data;
 
 // Global settings
 uint16_t config_port = 1043; // local port on which we listen for configuration
 // update commands
-IPAddress node_ip(152, 81, 10, 184); // IP address of the computer to which we
+IPAddress recipient_ip(152, 81, 10, 184); // IP address of the computer to which we
 // send the pulses
-uint16_t target_port = 1042; // port to which the pulses are sent
+uint16_t recipient_port = 1042; // port to which the pulses are sent
 uint32_t pulse_period = 500; // period of the pulse
 
 // Create an instance of the task scheduler
@@ -28,6 +31,7 @@ TickerScheduler scheduler(2);
 // Function prototypes
 void send_pulse();
 void update_configuration();
+void help_message(char* buffer);
 
 void setup(void)
 {
@@ -78,8 +82,13 @@ void setup(void)
     // Setup the scheduler
     if (!scheduler.add(0, pulse_period, send_pulse, true))
         Serial.println("ERROR: Could not create the pulse task");
-    // if (!scheduler.add(1, 1000, update_configuration, true))
-    //     Serial.println("ERROR: Could not create the configuration task");
+    if (!scheduler.add(1, 1000, update_configuration, true))
+        Serial.println("ERROR: Could not create the configuration task");
+
+    unsigned int voltage = analogRead(A0);
+    unsigned long milliseconds = millis();
+    pulse_seed = (voltage * milliseconds) % 4096;
+    Serial.printf("Seed for the pulses : %u\n", pulse_seed);
 }
 
 void loop()
@@ -92,13 +101,15 @@ void send_pulse()
 {
     char message[] = "tick";
 
+    pulse_seed = (pulse_seed + 1) % 65536;
+    sprintf(message, "%u", pulse_seed);
     // randomSeed(0);
-    long randNumber = random(4096);
-    Serial.printf("rand Arduino : %u\n", randNumber);
+    // long randNumber = random(4096);
+    // Serial.printf("aleatoire : %u\n", randNumber);
 
     int status = 0;
     // Send a packet to the ROS emergency stop gateway
-    status = Udp.beginPacket(node_ip, target_port);
+    status = Udp.beginPacket(recipient_ip, recipient_port);
     if (status == 0)
         Serial.println("Problen in IP or port for packet preparation");
     status = Udp.write(message, sizeof(message));
@@ -108,16 +119,16 @@ void send_pulse()
     status = Udp.endPacket();
     if (status == 0) {
         Serial.print("Could not send an UDP packet to IP ");
-        Serial.print(node_ip);
+        Serial.print(recipient_ip);
         Serial.print(" and port ");
-        Serial.println(target_port);
+        Serial.println(recipient_port);
     }
     // Serial.println("Sending the message");
     // Serial.println(message);
     // Serial.print("To IP ");
-    // Serial.print(node_ip);
+    // Serial.print(recipient_ip);
     // Serial.print(" and port ");
-    // Serial.println(target_port);
+    // Serial.println(recipient_port);
 }
 
 void update_configuration()
@@ -126,40 +137,94 @@ void update_configuration()
     int packet_size = Udp.parsePacket();
 
     if (packet_size) {
-        Serial.print("Received packet of size ");
-        Serial.println(packet_size);
-        Serial.print("From ");
-        IPAddress remote_ip = Udp.remoteIP();
-        Serial.print(remote_ip);
-        Serial.print(", port ");
-        Serial.println(Udp.remotePort());
 
         // Read the packet into packet_buffer
         int len = Udp.read(packet_buffer, 255);
         if (len > 0) {
             packet_buffer[len] = 0;
 
-            switch (packet_buffer[0]) {
-            case 'p': // update pulse period
-                if (len >= 2) {
-                    pulse_period = labs(atol(packet_buffer + 1));
+            // Special case: use wants to [r]ead the current settings
+            if (packet_buffer[0] == 'r') {
+                sprintf(packet_buffer,
+                    "Recipient IP   %s\n"
+                    "Recipient port %u\n"
+                    "Pulse period   %u\n",
+                    recipient_ip.toString().c_str(),
+                    recipient_port,
+                    pulse_period);
+            }
+            else if (len > 2) {
+                // Store the parameters in a String and remove empty characters
+                String parameter(packet_buffer + 1);
+                parameter.trim(); // remove front and trailing spaces but above
+                // all newline character
 
-                    bool status = scheduler.changePeriod(0, pulse_period) & scheduler.disable(0) & scheduler.enable(0);
+                // Select the command to execute based on first received
+                // character
+                switch (packet_buffer[0]) {
+                case 't': // update pulse period
+                {
+                    // retrieve the desired period
+                    uint32_t new_pulse_period = labs(parameter.toInt());
+
+                    // Change the period of the pulse task (change value and
+                    // update task)
+                    bool status = scheduler.changePeriod(0, new_pulse_period)
+                        & scheduler.disable(0) // required for the change to be
+                        & scheduler.enable(0); // effective
                     if (!status)
                         sprintf(packet_buffer, "ERROR: Could not change the pulse period\n");
-                    else
+                    else {
+                        pulse_period = new_pulse_period;
                         sprintf(packet_buffer, "New period : %u ms\n", pulse_period);
+                    }
+
+                    break;
                 }
-                else {
-                    sprintf(packet_buffer, "Syntax error on command: [p]eriod");
+                case 'a': // change the recipient address
+                {
+                    // attempt to update the recipient IP ( andchecking if the
+                    // IP is valid)
+                    if (!recipient_ip.fromString(parameter)) {
+                        sprintf(packet_buffer, "ERROR: Could not change the "
+                                               "recipient IP from string %s\n"
+                                               "Current address %s\n",
+                            parameter.c_str(), recipient_ip.toString().c_str());
+                    }
+                    else {
+                        sprintf(packet_buffer, "New recipient IP : %s\n",
+                            recipient_ip.toString().c_str());
+                    }
+
+                    break;
                 }
-                break;
-            case 'h': // TODO: print a help message with all accepted commands
-                sprintf(packet_buffer, "This should be an help message\n");
-                break;
-            default:
-                Serial.println("Contents:");
-                Serial.println(packet_buffer);
+                case 'p': // change the recipient port
+                {
+                    // retrieve the desired port number
+                    uint16_t new_port = labs(parameter.toInt());
+
+                    // check that the port is in the allowed range 0..65535
+                    // and use it
+                    if (new_port > 65535) {
+                        sprintf(packet_buffer, "ERROR: Could not change the "
+                                               "recipient port to%u\n",
+                            new_port);
+                    }
+                    else {
+                        recipient_port = new_port;
+                        sprintf(packet_buffer, "New recipient port : %u\n",
+                            recipient_port);
+                    }
+
+                    break;
+                }
+                case 'h': // TODO: print a help message with all accepted commands
+                default:
+                    help_message(packet_buffer);
+                }
+            }
+            else {
+                help_message(packet_buffer);
             }
         }
 
@@ -167,5 +232,24 @@ void update_configuration()
         Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
         Udp.write(packet_buffer);
         Udp.endPacket();
+
+        // Print the same reply to the serial line
+        Serial.print(packet_buffer);
     }
+}
+
+void help_message(char* buffer)
+{
+    sprintf(buffer,
+        "Commands are single-character. Accepted ones are\n"
+        "\tt change pulse period (time), in ms\n"
+        "\ta change recipient ip [a]ddress\n"
+        "\tp change recipient [p]ort\n"
+        "\tr read settings currently used.\n"
+        "Examples:\n"
+        "\tt 500           set pulse period to 500 ms\n"
+        "\ta 152.81.70.3   set recipient IP\n"
+        "\tp 1042          set recipient port to 1042\n"
+        "\nThe wifi SSID and password and network settings of the emergency stop"
+        " are hard-coded in the firmware.\n");
 }
